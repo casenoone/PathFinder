d3b055e5beaa2e16aada90332f329bdbc79f06de
casenoone <787502862@qq.com>
2020-08-09 16:58:23 +0800

    update

--- Pathfinding/pdf.h
+++ Pathfinding/pdf.h
@@ -11,17 +11,23 @@ public:
 	virtual vec3 generate() const = 0;
 };
 
-/*inline vec3 random_cosine_direction() {
-	auto r1 = random_double();
-	auto r2 = random_double();
-	auto z = sqrt(1 - r2);
 
-	auto phi = 2 * pi*r1;
-	auto x = cos(phi)*sqrt(r2);
-	auto y = sin(phi)*sqrt(r2);
+class uniform_pdf :public pdf {
 
-	return vec3(x, y, z);
-}*/
+public:
+	uniform_pdf(const vec3& w) { uvw.build_from_w(w); }
+
+	virtual double value(const vec3& direction) const override {
+		return 1 / (2 * pi);
+	}
+
+	virtual vec3 generate()const override {
+		return uvw.local(random_cosine_direction());
+	}
+public:
+	onb uvw;
+};
+
 
 class cosine_pdf : public pdf {
 public:
